a35a375dd4986608d1326151ba4e72c41a7cfcbf
EungDDing <ppoppo9802@naver.com>
2025-04-18 19:12:02 +0900

    Character data update

---
+++ Assets/03Data/TableEntity.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class CharacterData_Entity
+{
+    public int ID;
+    public string Name;
+    public int Rank;
+    public string Weapon;
+    public string Role;
+    public string Skill;
+    public int SkillCost;
+    public string AttackType;
+    public string DefenceType;
+    public string Position;
+}
+
+[System.Serializable]
+public class LevelData_Entity
+{
+    public int Level;
+    public int HP;
+    public int Armor;
+    public int Attack;
+    public int Exp;
+}
